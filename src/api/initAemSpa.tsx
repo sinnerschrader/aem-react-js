aca9670dd175722f8ccf608c092017e500427ca8
stefan meyer <stefan.meyer@sinnerschrader.com>
2018-06-29 12:59:06 +0200

    fix: starting spa

--- src/api/initAemSpa.tsx
+++ src/api/initAemSpa.tsx
@@ -1,10 +1,18 @@
-import {Constants, Utils} from '@adobe/cq-react-editable-components';
+import {
+  ComponentMapping,
+  Constants,
+  MapTo,
+  SpaComponentProps,
+  Utils
+} from '@adobe/cq-react-editable-components';
 import * as React from 'react';
+import * as ReactDom from 'react-dom';
 import {IncludeProps} from '../ResourceInclude';
 import {RootComponentRegistry} from '../RootComponentRegistry';
 import {Include} from '../compatibility/Include';
 import {MapVanillaTo} from '../compatibility/ModelContext';
 import {Compatibility, Config} from './Compatibility';
+import {mapping} from './Mapping';
 
 export const rootRegistry = new RootComponentRegistry();
 
@@ -16,6 +24,26 @@ export class AemSpaCompatibility implements Compatibility {
   public get include(): React.ComponentClass<IncludeProps> {
     return Include;
   }
+
+  public start(
+    pagePath: string,
+    dataPath: string,
+    type: string,
+    el: HTMLElement
+  ): void {
+    const Com = ComponentMapping.get(type);
+    const props: SpaComponentProps = {
+      cq_model_data_path: dataPath,
+      cq_model_page_path: pagePath,
+      cq_model: null
+    };
+    ReactDom.render(<Com {...props} />, el);
+  }
+
+  public map(c: React.ComponentClass): React.ComponentClass {
+    return MapTo('bullshit')(c, null);
+  }
+
   public get dndContainer():
     | React.ComponentClass<IncludeProps>
     | React.StatelessComponent<IncludeProps> {
@@ -29,9 +57,13 @@ export class AemSpaCompatibility implements Compatibility {
   public isInEditor(): boolean {
     return Utils.isInEditor();
   }
-  public mapTo(resourceType: string, config: Config<{}>): void {
-    MapVanillaTo(resourceType, config.componentClass, config.editConfig);
-  }
 }
 
-export const initAemSpa = () => new AemSpaCompatibility();
+export const initAemSpa = (): AemSpaCompatibility => {
+  Object.keys(mapping).forEach((type: string) => {
+    const config = mapping[type];
+    MapVanillaTo(type, config.componentClass, config.editConfig);
+  });
+
+  return new AemSpaCompatibility();
+};
